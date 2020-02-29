07480bc73db1bde9bf9e50a24bde6d51bb72d7ce
Musppelheim <neloliver@hotmail.com>
2020-02-29 08:26:07 +0700

    newAdded

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public static double calculate(int weight, double height) {
-         weight = 82;
-         height = 1.82;
+    public double calculate(double height, int weight) {
         double Bmi = weight / (height * height);
         return Bmi;
     }
--- src/MainV2.java
+++ src/MainV2.java
@@ -1,8 +1,10 @@
-public class Main {
+public class MainV2 {
     public static void main(String[] args) {
+        BmiService service = new BmiService();
         double height = 1.82;
         int weight = 82;
-        double Bmi = weight / (height * height);
+        double Bmi = service.calculate(height, weight);
         System.out.println(Bmi);
     }
 }
+
